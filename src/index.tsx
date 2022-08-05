4e5433d7c4aa425779cf1dcbe4041c4ec49bb33b
Mohsen Fallahnejad <mohsenfallahnjd@gmail.com>
2022-08-05 19:56:18 +0430

    Add stylelint & eslint & ts & react router and files structure

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
+
+reportWebVitals();
+
+serviceWorker.unregister();
